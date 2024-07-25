e86c745615b48c27453a97e1e95db8b053b3e7d7
MyslivchenkoRI <MyslivchenkoRI@aeropribor.ru>
2024-07-25 17:58:20 +0300

    Update

--- debug or release build/code.cpp
+++ debug or release build/code.cpp
@@ -1,6 +1,6 @@
 #ifdef QT_NO_DEBUG
-	qDebug() << "QT_NO_DEBUG";
+	qDebug() << "QT_NO_DEBUG";  //сработет если сборка "Выпуск"
 #endif
 #ifdef QT_DEBUG
-	qDebug() << "QT_DEBUG";
+	qDebug() << "QT_DEBUG";		//сработет если сборка "Отладка"
 #endif
