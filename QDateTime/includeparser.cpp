da43ac512a9e21945f094b713afa46d0a3c6a558
MyslivchenkoRI <MyslivchenkoRI@aeropribor.ru>
2024-08-23 11:57:19 +0300

    Update

--- QDateTime/includeparser.cpp
+++
@@ -1,1 +0,0 @@
-ui->tableWidget->setItem(insInd, 1, new QTableWidgetItem(d.lastModified().toString("yyyy.MM.dd hh:mm:ss")));
---
+++ QDateTime/текущее и форматированный вывод.cpp
@@ -0,0 +1,3 @@
+QDateTime curDateTime = QDateTime::currentDateTime();
+QDateTime someDateTime = curDateTime;
+qdbg << someDateTime.toString("yyyy.MM.dd hh:mm:ss");
