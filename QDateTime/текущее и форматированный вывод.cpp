3c9fe979c710447df357c43de7ba97dfb7159a9f
MyslivchenkoRI <MyslivchenkoRI@aeropribor.ru>
2024-08-26 12:17:28 +0300

    Update

--- QDateTime/текущее и форматированный вывод.cpp
+++ QDateTime/текущее и форматированный вывод.cpp
@@ -1,3 +1,6 @@
 QDateTime curDateTime = QDateTime::currentDateTime();
 QDateTime someDateTime = curDateTime;
 qdbg << someDateTime.toString("yyyy.MM.dd hh:mm:ss");
+
+// внимание! чтобы добавлять в имя файла не забыть убрать двоеточия!!!
+qdbg << someDateTime.toString("yyyy.MM.dd hh_mm_ss");
