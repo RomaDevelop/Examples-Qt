f87f16f2cfa7288f4e4e37eb7428f62c7e0631c1
MyslivchenkoRI <MyslivchenkoRI@aeropribor.ru>
2024-08-27 16:50:28 +0300

    Update

--- QAction (контекстное меню)/code.cpp
+++ QAction (контекстное меню)/code.cpp
@@ -1,7 +1,23 @@
-	QAction *mOpen = new QAction("mOpen", ui->tableWidget);
-	QAction *mShowInExplorer = new QAction("mShowInExplorer", ui->tableWidget);
-	ui->tableWidget->addAction(mOpen);
-	ui->tableWidget->addAction(mShowInExplorer);
-	ui->tableWidget->setContextMenuPolicy(Qt::ActionsContextMenu);
-	connect(mOpen, &QAction::triggered, [](){qdbg << "mOpen";});
-	connect(mShowInExplorer, &QAction::triggered, [](){qdbg << "mShowInExplorer";});
+//Policy Qt::ActionsContextMenu
+
+QAction *mOpen = new QAction("mOpen", ui->tableWidget);
+QAction *mShowInExplorer = new QAction("mShowInExplorer", ui->tableWidget);
+ui->tableWidget->addAction(mOpen);
+ui->tableWidget->addAction(mShowInExplorer);
+ui->tableWidget->setContextMenuPolicy(Qt::ActionsContextMenu);
+connect(mOpen, &QAction::triggered, [](){qdbg << "mOpen";});
+connect(mShowInExplorer, &QAction::triggered, [](){qdbg << "mShowInExplorer";});
+	
+//Policy Qt::CustomContextMenu
+treeWidget->setContextMenuPolicy(Qt::CustomContextMenu); // Устанавливаем политику контекстного меню
+
+QMenu menu(treeWidget);
+QObject::connect(&menu, &QMenu::triggered, [](QAction *action){qDebug() << "QMenu::triggered " << action->text();});
+
+QAction *myAction = menu.addAction("Мое действие");
+QObject::connect(myAction, &QAction::triggered, [](){qDebug() << "myAction";});
+
+QObject::connect(treeWidget, &QTreeWidget::customContextMenuRequested, [&](const QPoint& pos) {
+		qDebug() << "Контекстное меню вызвано";
+		menu.exec(treeWidget->mapToGlobal(pos));
+	});
