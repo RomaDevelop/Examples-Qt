d6b224213729a39b6cbd51fd223b49d032533d3e
MyslivchenkoRI <MyslivchenkoRI@aeropribor.ru>
2025-01-23 16:30:45 +0300

    Update

--- QDateTime/текущее и форматированный вывод.cpp
+++ QDateTime/текущее и форматированный вывод.cpp
@@ -3,5 +3,6 @@ QDateTime someDateTime = curDateTime;
 qdbg << someDateTime.toString("yyyy.MM.dd hh:mm:ss"); // внимание, тут есть двоеточия! не годится в путь к папке!
 qdbg << someDateTime.toString("yyyy.MM.dd hh:mm:ss.zzz"); // + микросекунды внимание, тут есть двоеточия! не годится в путь к папке!
 
-// внимание! чтобы добавлять в имя файла не забыть убрать двоеточия!!!
-qdbg << someDateTime.toString("yyyy.MM.dd hh_mm_ss");
+// внимание! чтобы добавлять в имя файла не забыть убрать двоеточия из времени!!!
+qdbg << someDateTime.toString("yyyy.MM.dd hh_mm_ss_zzz");
+qdbg << someDateTime.toString("yyyy.MM.dd hh-mm-ss-zzz");
