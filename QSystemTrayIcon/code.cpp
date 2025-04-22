86fdd3918b519129feab21e2fd84f43e790598ad
MyslivchenkoRI <MyslivchenkoRI@aeropribor.ru>
2025-04-22 17:16:13 +0300

    Update

--- QSystemTrayIcon/code.cpp
+++ QSystemTrayIcon/code.cpp
@@ -7,7 +7,18 @@ void TimerWindow::CreateTrayIcon()
 	icon->setIcon(QApplication::style()->standardIcon(QStyle::SP_BrowserReload));
 	icon->setToolTip("Timer");
 	icon->show();
-	connect(icon, &QSystemTrayIcon::activated, [this](){
-		ShowMainWindow();
+	
+	QMenu *menu = new QMenu(this);
+	icon->setContextMenu(menu);
+	
+	connect(icon, &QSystemTrayIcon::activated, [icon, showFoo](QSystemTrayIcon::ActivationReason reason){
+		if(reason == QSystemTrayIcon::Trigger) show();
+		if(reason == QSystemTrayIcon::Context) icon->contextMenu()->exec();
 	});
+	
+	menu->addAction("Show Notes");
+	connect(menu->actions().back(), &QAction::triggered, showFoo);
+
+	menu->addAction("Create new note");
+	connect(menu->actions().back(), &QAction::triggered, [this](){ SlotCreationNewNote(); });
 }
