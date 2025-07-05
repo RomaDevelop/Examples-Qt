ccc695e98804b15c997a4238d864c847b71c7b87
MyslivchenkoRI <MyslivchenkoRI@aeropribor.ru>
2025-07-05 18:06:44 +0300

    Update

---
+++ QTableWidget/создание виджетов в клетках - выживание.cpp
@@ -0,0 +1,37 @@
+# ВИДЖЕТ НЕВОЗМОЖНО ИЗЪЯТЬ ИЗ ТАБЛИЦЫ
+# ЕСЛИ НА МЕСТО СУЩЕСТВУЮЩЕГО ВИДЖЕТА ЗАПИСАТЬ НОВЫЙ, СТАРЫЙ УНИЧТОЖАЕТСЯ
+# Но, казалось бы, можно перемещать виджеты. Казалось бы даже из одной таблицы в другую.
+# Но, ****, если на старое место виджета записать новый он, ****, умирает. Даже если он, ****, в другой, ****, таблице!!!
+
+	auto t = new QTableWidget;
+	t->show();
+	t->setRowCount(5); t->setColumnCount(6);
+	t->setCellWidget(0,0, new QLabel("0"));
+	t->setCellWidget(0,1, new QLabel("1"));
+	t->setCellWidget(0,2, new QLabel("2"));
+	t->setCellWidget(0,3, new QLabel("3"));
+	t->setCellWidget(0,4, new QLabel("4"));
+	t->setCellWidget(0,5, new QLabel("5"));
+
+	QObject::connect(t->cellWidget(0,0), &QWidget::destroyed, [](){ qdbg << 0; });
+	QObject::connect(t->cellWidget(0,1), &QWidget::destroyed, [](){ qdbg << 1; });
+	QObject::connect(t->cellWidget(0,2), &QWidget::destroyed, [](){ qdbg << 2; });
+	QObject::connect(t->cellWidget(0,3), &QWidget::destroyed, [](){ qdbg << 3; });
+	QObject::connect(t->cellWidget(0,4), &QWidget::destroyed, [](){ qdbg << 4; });
+	QObject::connect(t->cellWidget(0,5), &QWidget::destroyed, [](){ qdbg << 5; });
+
+	t->setCellWidget(0,0,new QLabel("0.1"));  // 0 будет уничтожен
+
+	auto w = new QWidget;
+	t->cellWidget(0,1)->setParent(w);
+	t->setCellWidget(0,1,new QLabel("1.1")); // 1 все равно уничтожен
+
+	auto t2 = new QTableWidget;
+	t2->setColumnCount(10); t2->setRowCount(1);
+	t2->show();
+
+	t2->setCellWidget(0,0, t->cellWidget(0,2)); // 2 ВЫЖИВАЕТ
+	t2->setCellWidget(0,1, t2->cellWidget(0,0)); // 2 УМИРАЕТ
+
+	t2->setCellWidget(0,1, t->cellWidget(0,3));
+	t2->setCellWidget(0,1, new QLabel("3.1")); // 3 уничтожен
--- QTableWidget/создание виджетов в клетках.cpp
+++ QTableWidget/создание виджетов в клетках.cpp
@@ -1,14 +1,35 @@
-	
-	// один виджет
-	tableWidget->setCellWidget(0,0,new QPushButton("test"));
-	qDebug() << ((QPushButton*)tableWidget->cellWidget(0,0))->text();
-	
-	// группа
-	QWidget *w = new QWidget;
-	QHBoxLayout *hLao = new QHBoxLayout(w);
-	hLao->setSpacing(0); // убтирает расстояние между виджетами внутри Layout
-	hLao->setContentsMargins(0,0,0,0); // убтирает расстояние до края
-	hLao->addWidget(new QLineEdit);
-	hLao->addWidget(new QPushButton("test2"));
 
-	ui->tableWidget->setCellWidget(1,0,w);
+// один виджет
+tableWidget->setCellWidget(0,0,new QPushButton("test"));
+qDebug() << ((QPushButton*)tableWidget->cellWidget(0,0))->text();
+
+// группа
+QWidget *w = new QWidget;
+QHBoxLayout *hLao = new QHBoxLayout(w);
+hLao->setSpacing(0); // убтирает расстояние между виджетами внутри Layout
+hLao->setContentsMargins(0,0,0,0); // убтирает расстояние до края
+hLao->addWidget(new QLineEdit);
+hLao->addWidget(new QPushButton("test2"));
+
+ui->tableWidget->setCellWidget(1,0,w);
+
+# ВИДЖЕТ НЕВОЗМОЖНО ИЗЪЯТЬ ИЗ ТАБЛИЦЫ
+# ЕСЛИ НА МЕСТО СУЩЕСТВУЮЩЕГО ВИДЖЕТА ЗАПИСАТЬ НОВЫЙ, СТАРЫЙ УНИЧТОЖАЕТСЯ
+# Но, казалось бы, можно перемещать виджеты. Казалось бы даже из одной таблицы в другую.
+# Но, ****, если на старое место виджета записать новый он, ****, умирает. Даже если он, ****, в другой, ****, таблице!!!
+
+	auto t = new QTableWidget;
+	t->show();
+	t->setRowCount(5);
+	t->setColumnCount(6);
+	t->setCellWidget(0,0,new QLabel("0"));
+	t->setCellWidget(0,1,new QLabel("1"));
+
+	QObject::connect(t->cellWidget(0,0), &QWidget::destroyed, [](){ qdbg << 0; });
+	QObject::connect(t->cellWidget(0,1), &QWidget::destroyed, [](){ qdbg << 1; });
+
+	t->setCellWidget(0,0,new QLabel("0.1"));  // 0 будет уничтожен
+
+	auto w = new QWidget;
+	t->cellWidget(0,1)->setParent(w);
+	t->setCellWidget(0,1,new QLabel("1.1")); // 1 все равно уничтожен
