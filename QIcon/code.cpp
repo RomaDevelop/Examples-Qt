59bb28b54e837019f8e02d83375459a935b05124
MyslivchenkoRI <MyslivchenkoRI@aeropribor.ru>
2026-04-04 19:05:51 +0300

    fix error

--- QIcon/code.cpp
+++ QIcon/code.cpp
@@ -1,3 +1,3 @@
-btn>setIcon(QApplication::style()->standardIcon(QStyle::StandardPixmap::SP_FileDialogDetailedView));
+btn->setIcon(QApplication::style()->standardIcon(QStyle::StandardPixmap::SP_FileDialogDetailedView));
 
 MyQDialogs::ShowAllStandartIcons();
